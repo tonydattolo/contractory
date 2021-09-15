1ff7452c4c052e87da073dfecf3f19d83e7197e1
Tony Dattolo <anthonydattolo@gmail.com>
2021-09-15 00:46:52 -0400

    sidenav style updates for large screens

--- frontend/components/Sidenav.js
+++ frontend/components/Sidenav.js
@@ -4,7 +4,7 @@ import { Nav, Button } from 'react-bootstrap'
 import Link from "next/dist/client/link";
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAddressBook, faAt, faHouseUser } from '@fortawesome/free-solid-svg-icons'
+import { faAddressBook, faAt, faGem, faHome, faUser, faUsers, faSearch, faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 import styles from '../styles/modules/Sidenav.module.scss'
 
@@ -12,70 +12,92 @@ export default function Sidenav() {
   return (
     <Nav defaultActiveKey="/home" className={`flex-column ${styles.nav}`}>
       
-      <Nav.Item as="li">
+      <Nav.Item as="li" className={styles.logo}>
         <Link href="/" passHref>
           <Nav.Link href="#">
-            <FontAwesomeIcon icon={faHouseUser} />
-            Home
+            <FontAwesomeIcon size='2x' icon={faGem} />
           </Nav.Link>
         </Link>
       </Nav.Item>
       
-      <Nav.Item as="li">
+      <Nav.Item as="li" className={styles.sidenavItem}>
         <Link href="/" passHref>
-          <Nav.Link href="#">
-            <FontAwesomeIcon icon={faAddressBook} />
-            Profile
+          <Nav.Link href="#" className={styles.navLink}>
+            <div className={styles.navIconContainer}>
+              <FontAwesomeIcon icon={faHome} />
+            </div>
+            <span className={styles.linkText}>Home</span>
           </Nav.Link>
         </Link>
       </Nav.Item>
       
-      <Nav.Item as="li">
+      <Nav.Item as="li" className={styles.sidenavItem}>
         <Link href="/" passHref>
-          <Nav.Link href="#">
-            <FontAwesomeIcon icon={faAddressBook} />
-            Groups
+          <Nav.Link href="#" className={styles.navLink}>
+            <div className={styles.navIconContainer}>
+              <FontAwesomeIcon icon={faUser} />
+            </div>
+            <span className={styles.linkText}>Profile</span>
           </Nav.Link>
         </Link>
       </Nav.Item>
       
-      <Nav.Item as="li">
+      <Nav.Item as="li" className={styles.sidenavItem}>
         <Link href="/" passHref>
-          <Nav.Link href="#">
-            <FontAwesomeIcon icon={faAt} />
-            Notifications
+          <Nav.Link href="#" className={styles.navLink}>
+            <div className={styles.navIconContainer}>
+              <FontAwesomeIcon icon={faUsers} />
+            </div>
+            <span className={styles.linkText}>Groups</span>
           </Nav.Link>
         </Link>
       </Nav.Item>
       
-      <Nav.Item as="li">
+      <Nav.Item as="li" className={styles.sidenavItem}>
         <Link href="/" passHref>
-          <Nav.Link href="#">
-            <FontAwesomeIcon icon={faAddressBook} />
-            Explore
+          <Nav.Link href="#" className={styles.navLink}>
+            <div className={styles.navIconContainer}>
+              <FontAwesomeIcon icon={faAt} />
+            </div>
+            <span className={styles.linkText}>Notifications</span>
           </Nav.Link>
         </Link>
       </Nav.Item>
       
-      <Nav.Item as="li">
+      <Nav.Item as="li" className={styles.sidenavItem}>
         <Link href="/" passHref>
-          <Nav.Link href="#">
-            <FontAwesomeIcon icon={faAddressBook} />
-            Messages
+          <Nav.Link href="#" className={styles.navLink}>
+            <div className={styles.navIconContainer}>
+              <FontAwesomeIcon icon={faSearch} />
+            </div>
+            <span className={styles.linkText}>Explore</span>
           </Nav.Link>
         </Link>
       </Nav.Item>
       
-      <Nav.Item as="li">
+      <Nav.Item as="li" className={styles.sidenavItem}>
         <Link href="/" passHref>
-          <Nav.Link href="#">
-            <FontAwesomeIcon icon={faAddressBook} />
-            Lists
+          <Nav.Link href="#" className={styles.navLink}>
+            <div className={styles.navIconContainer}>
+              <FontAwesomeIcon icon={faEnvelope} />
+            </div>
+            <span className={styles.linkText}>Messages</span>
+          </Nav.Link>
+        </Link>
+      </Nav.Item>
+      
+      <Nav.Item as="li" className={styles.sidenavItem}>
+        <Link href="/" passHref>
+          <Nav.Link href="#" className={styles.navLink}>
+            <div className={styles.navIconContainer}>
+              <FontAwesomeIcon icon={faAddressBook} />
+            </div>
+            <span className={styles.linkText}>Lists</span>
           </Nav.Link>
         </Link>
       </Nav.Item>
 
-      <Button size='lg' className={styles.btn}>
+      <Button size='lg' className={styles.postButton}>
         Post
       </Button>
       
