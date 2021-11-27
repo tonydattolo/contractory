60beeb289f20e872675537ad4631f856363f764d
Tony Dattolo <anthonydattolo@gmail.com>
2021-11-26 22:39:26 -0500

    add admin settings for contracts models

--- backend2/contracts/admin.py
+++ backend2/contracts/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
+from .models import SmartContract, Clause, Party
 # Register your models here.
+
+class ContractAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'created_at', 'updated_at')
+    list_filter = ('created_at', 'updated_at')
+    search_fields = ('name', 'description')
+
+
+class ClauseAdmin(admin.ModelAdmin):
+    list_display = ('id', 'contract', 'content', 'created_at', 'updated_at')
+    list_filter = ('created_at', 'updated_at')
+    search_fields = ('name', 'contract', 'content')
+
+class PartyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'party', 'address', 'description', 'updated_at')
+    list_filter = ('created_at', 'updated_at')
+    search_fields = ('name', 'description')
+
+admin.site.register(SmartContract, ContractAdmin)
+admin.site.register(Clause, ClauseAdmin)
+admin.site.register(Party, PartyAdmin)
--- backend2/contracts/models.py
+++ backend2/contracts/models.py
@@ -11,7 +11,6 @@ class SmartContract(models.Model):
     """
     This class represents the SmartContract model.
     """
-
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     owner = models.ForeignKey(USER, on_delete=CASCADE)
     name = models.CharField(max_length=100)
@@ -21,9 +20,9 @@ class SmartContract(models.Model):
     address = models.CharField(max_length=100)
     # address = models.CharField(max_length=42, unique=True, validators=[ethereumAddressValidator])
 
-    abi = models.TextField(blank=True)
-    bytecode = models.TextField(blank=True)
-
+    # abi = models.TextField(blank=True)
+    # bytecode = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     status_choices = [
@@ -35,7 +34,7 @@ class SmartContract(models.Model):
     status = models.CharField(choices=status_choices, default='draft', max_length=20)
 
     # list here for all authorized parties to read and/or write contracts
-    authorized_parties = models.ManyToManyField(USER, related_name='authorized_parties')
+    # authorized_parties = models.ManyToManyField(USER, related_name='authorized_parties')
     
     
     
@@ -45,23 +44,15 @@ class SmartContract(models.Model):
     # viewingParties = models.ManyToManyField('ViewingParty', blank=True)
     # lawyers = models.ManyToManyField('Lawyer', blank=True)
 
-class Clause(models.Model):
-    """
-    This class represents the Clause model.
-    """
-    contract = models.ForeignKey(SmartContract, on_delete=CASCADE)
-    content = models.TextField(blank=True)
-    amount = models.IntegerField(default=0)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    
     
 
 class Party(models.Model):
     """
     This class represents the Party model.
     """
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     party = models.ForeignKey(USER, on_delete=CASCADE)
+    description = models.TextField(blank=True)
     address = models.CharField(max_length=42, unique=True, validators=[ethereumAddressValidator])
     
     created_at = models.DateTimeField(auto_now_add=True)
@@ -75,3 +66,52 @@ class Party(models.Model):
     ]
     role = models.CharField(choices=ROLES, default='sender', max_length=12)
     
+class Clause(models.Model):
+    """
+    This class represents the Clause model.
+    """
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    contract = models.ForeignKey(SmartContract, on_delete=CASCADE)
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # amount = models.IntegerField(default=0)
+    # sender = models.ForeignKey(Party, on_delete=CASCADE, related_name='sender')
+    # receiver = models.ForeignKey(Party, on_delete=CASCADE, related_name='receiver')
+    # trigger_choices = [
+    #     ('oracle', 'oracle'),
+    #     ('manual', 'manual'),
+    #     ('time', 'time'),
+    # ]
+    # trigger = models.CharField(choices=trigger_choices, default='manual', max_length=20)
+    # trigger_at = models.DateTimeField(blank=True, null=True)
+    # status_choices = [
+    #     ('pending', 'pending'),
+    #     ('complete', 'complete'),
+    # ]
+    # status = models.CharField(choices=status_choices, default='pending', max_length=20)
+
+    # payment_period_unit_choices = [
+    #     ('day', 'day'),
+    #     ('week', 'week'),
+    #     ('month', 'month'),
+    #     ('year', 'year'),
+    #     ('one_time', 'one_time'),
+    #     ('none', 'none'),
+    # ]
+    # payment_period = models.IntegerField(default=0)
+    # payment_period_unit = models.CharField(max_length=20, default='days')
+    # payment_amount = models.IntegerField(default=0)
+    # payment_currency = models.CharField(max_length=20, default='ETH')
+    # payment_address = models.CharField(max_length=42, unique=True, validators=[ethereumAddressValidator])
+    # payment_status_choices = [
+    #     ('pending', 'pending'),
+    #     ('complete', 'complete'),
+    # ]
+    # payment_status = models.CharField(choices=payment_status_choices, default='pending', max_length=20)
+    # payment_tx_hash = models.CharField(max_length=100, blank=True)
+    # payment_tx_block_number = models.IntegerField(default=0)
+    # payment_tx_block_hash = models.CharField(max_length=100, blank=True)
+    # payment_tx_timestamp = models.DateTimeField(blank=True, null=True)
+    # payment_tx_confirmations = models.IntegerField(default=0)
