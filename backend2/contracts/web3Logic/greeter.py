4dea77f115b34a7449782de3e9810942d9c353b3
Tony Dattolo <anthonydattolo@gmail.com>
2021-12-13 04:34:34 -0500

    successfully interacted with contract on local ganache blockchain

--- backend2/contracts/web3Logic/greeter.py
+++ backend2/contracts/web3Logic/greeter.py
@@ -2,11 +2,11 @@ from web3 import Web3
 from solcx import compile_source, compile_standard
 import json
 import os
-from dotenv import load_dotenv
-load_dotenv()
+# from dotenv import load_dotenv
+# load_dotenv()
 
 GANACHE_URL = "HTTP://127.0.0.1:7545"
-CHAIN_ID = 5777
+CHAIN_ID = 1337
 
 # timestamp tutorial for using solc https://www.youtube.com/watch?v=M576WGiDBdQ&t=10867s
 # compiled_sol = compile_source(
@@ -56,8 +56,8 @@ abi = compiled_sol["contracts"]["Greeter.sol"]["Greeter"]["abi"]
 
 w3 = Web3(Web3.HTTPProvider(GANACHE_URL))
 
-test_address = "0xC805A751470F4E5684048ea53008654b89Cc505B"
-test_address_private_key = "0x041e223752afea77f424cd8921d8ce7040d8fc2a8df6489835afe98dbadc3e16"
+test_address = "0xf98c6f6b64CF53f5244f3702dbC122798ECcf3B6"
+test_address_private_key = "0x981a5ced5561ff1c2c8798092aa8be6b09110fa551367fcb29fda142a4d3220c"
 # test_address_private_key_fromENV = os.getenv("TEST_ADDRESS_PRIVATE_KEY")
 
 w3.eth.defaultAccount = w3.eth.accounts[0]
@@ -67,25 +67,27 @@ nonce = w3.eth.getTransactionCount(w3.eth.defaultAccount)
 
 tx = Greeter.constructor().buildTransaction({
     'chainId': CHAIN_ID,
-    'from': w3.eth.defaultAccount,
+    'from': test_address,
     'nonce': nonce,
-    # 'gas': 1000000,
-    # 'gasPrice': w3.toWei('50', 'gwei'),
+    'gasPrice': w3.eth.gas_price,
 })
 signed_tx = w3.eth.account.signTransaction(tx, private_key=test_address_private_key)
 
+tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
+tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
 
 
+# Working with a contract you need: address and abi
+# Call -> simulates making the call and returning the value but no state change
+# transact -> actually makes a state change
+
+greeter = w3.eth.contract(
+    address=tx_receipt.contractAddress,
+    abi=abi
+)
 # tx_hash = Greeter.constructor().transact()
-# tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+print(greeter.functions.greet().call())
 
-# greeter = w3.eth.contract(
-#     address=tx_receipt.contractAddress,
-#     abi=abi
-#     )
-
-# greeter.functions.greet().call()
-
-# tx_hash = greeter.functions.setGreeting('Ciao Bella').transact()
-# tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
-# greeter.functions.greet().call()
+tx_hash = greeter.functions.setGreeting('Ciao Bella').transact()
+tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+print(greeter.functions.greet().call())
